3e4cb49237b2b3ac69f013f96f46c85e07c6f849
olrlozl <thakd221@naver.com>
2023-05-24 22:25:38 +0900

    Section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import ipads from '../data/ipads.js'
+
 //// 장바구니!
 // 1. 장바구니 아이콘 클릭하면 드롭다운 메뉴가 나타나도록.
 // 2. 드롭다운 메뉴 이외의 화면을 클릭하면, 드롭다운 메뉴가 사라지도록.
@@ -32,7 +34,7 @@ function hideBasket() {
 }
 
 
-//// 검색!
+//// 헤더 검색!
 // 1. 검색아이콘serach-starter 클릭시, 검색바 보이기
 // 2. 닫기아이콘search-closer 또는 배경그림자shadow 클릭시, 검색바 숨기기
 // 3. 검색바가 나타나면 더이상 스크롤 되지 않도록 하기
@@ -80,3 +82,64 @@ function hideSearch() {
   searchDelayEls.reverse()
   searchInputEl.value = ''
 }
+
+
+//// 요소의 가시성 관찰
+// .info가 화면에 보일때, 아래에서 위로 올라오며 보이도록 효과주기.
+const io = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return
+    }
+    entry.target.classList.add('show')
+  })
+})
+const infoEls = document.querySelectorAll('.info')
+infoEls.forEach(function (el) {
+  io.observe(el)
+})
+
+
+//// 비디오 재생!
+const video = document.querySelector('.stage video')
+const playBtn = document.querySelector('.stage .controller--play')
+const pauseBtn = document.querySelector('.stage .controller--pause')
+
+playBtn.addEventListener('click', function () {
+  video.play()
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+})
+pauseBtn.addEventListener('click', function () {
+  video.pause()
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+})
+
+
+//// '당신에게 맞는 iPad는?' 렌더링!
+const itemsEl = document.querySelector('section.compare .items')
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement('div')
+  itemEl.classList.add('item')
+
+  let colorList = ''
+  ipad.colors.forEach(function (color) {
+    colorList += `<li style="background-color: ${color}"></li>`
+  })
+
+  itemEl.innerHTML = /*html*/`
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt=${ipad.name} />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString('en-US')}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+    `
+    itemsEl.append(itemEl)
+})
